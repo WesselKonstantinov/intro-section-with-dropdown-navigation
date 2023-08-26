b03766ce2c2c2b6bc394c722fb8732fc01472497
Wessel Konstantinov <wes_kon1@hotmail.com>
2023-08-26 19:31:39 +0200

    Enable closing menu using Escape

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ navToggle.addEventListener("click", () => {
   const isOpen = navToggle.getAttribute("aria-expanded") === "true";
   navToggle.setAttribute("aria-expanded", !isOpen);
 });
+
+const nav = document.querySelector(".js-nav");
+nav.addEventListener("keyup", (e) => {
+  if (e.code === "Escape") {
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
